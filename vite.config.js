ae29107ade9e0c9d4ad81be61175531b7bd6e513
Carol Marcel <cg.marcel@gmail.com>
2025-07-13 17:38:30 -0400

    Modificado

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/EVALUACION_REACT/', // 👈 IMPORTANTE: nombre exacto del repositorio
+  base: '/EVALUACION_REACT/', // 👈 nombre del repo en GitHub
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsInlineLimit: 0, // Evita que algunos archivos se incrusten en el HTML
+  },
+  server: {
+    headers: {
+      'Cache-Control': 'no-store', // Desactiva caché en desarrollo
+    }
+  }
 })
